0b68d8a887ee9ae88e027e79cd67fa5562690e02
moustafa elsayed <moelsayed949@gmail.com>
2024-02-17 18:31:48 +0200

    add theme

--- App.jsx
+++ App.jsx
@@ -14,8 +14,8 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:"center",
-    alignItems:"center"
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     color: 'red',
