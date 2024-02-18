055725a2e97644006066fd964a4c40abbc00c103
moustafa elsayed <moelsayed949@gmail.com>
2024-02-18 21:12:45 +0200

    add tailwind classes

--- App.jsx
+++ App.jsx
@@ -1,10 +1,13 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import AppNavigations from './src/appnavigation/AppNavigations';
 
 const App = () => {
   return (
- <AppNavigations />
+     <AppNavigations />
+    // <View className="bg-white h-full w-full">
+    //   <Text>sgkhgdkhd</Text>
+    // </View>
   );
 };
 
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View className="bg-black h-full w-full">
       <StatusBar
         style="light"
         animated={true}
         translucent={true}
         backgroundColor="transparent"
       />
-      <Image blurRadius={20} source={require('../images/bg.jpg')} />
-      <Text>kdkfhk;</Text>
+      {/* <Image blurRadius={20} source={require('../images/bg.jpg')} /> */}
+      <Text className="text-yellow-400">kdkfhk;</Text>
+      <Text className="text-white">kdkfhk;</Text>
     </View>
   );
 };
