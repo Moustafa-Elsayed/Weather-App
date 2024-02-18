618ad5bb3ac13381ec32d5b2f02e9f5e3b481b2c
moustafa elsayed <moelsayed949@gmail.com>
2024-02-18 21:28:15 +0200

    add items

--- App.jsx
+++ App.jsx
@@ -1,13 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet,} from 'react-native';
 import React from 'react';
 import AppNavigations from './src/appnavigation/AppNavigations';
 
 const App = () => {
   return (
      <AppNavigations />
-    // <View className="bg-white h-full w-full">
-    //   <Text>sgkhgdkhd</Text>
-    // </View>
+  
   );
 };
 
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -3,16 +3,14 @@ import React from 'react';
 
 const HomeScreen = () => {
   return (
-    <View className="bg-black h-full w-full">
+    <View>
       <StatusBar
         style="light"
         animated={true}
         translucent={true}
         backgroundColor="transparent"
       />
-      {/* <Image blurRadius={20} source={require('../images/bg.jpg')} /> */}
-      <Text className="text-yellow-400">kdkfhk;</Text>
-      <Text className="text-white">kdkfhk;</Text>
+      <Image blurRadius={30} source={require('../images/bg.jpg')} />
     </View>
   );
 };
