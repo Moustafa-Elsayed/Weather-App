944d3454c0d66ac89f2cbcf6babddcb62e2d61a0
moustafa elsayed <moelsayed949@gmail.com>
2024-02-20 01:46:28 +0200

    fetch api

--- src/api/weather.js
+++ src/api/weather.js
@@ -6,3 +6,25 @@ const forecastEndPiont =
 `;
 const searchEndPiont = params =>
   `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityNAme}`;
+
+const apicall = async (endPoint, params) => {
+  const options = {
+    method: 'GET',
+    url: endPoint,
+  };
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+    return {};
+  }
+};
+
+export const fetchForcatdata = params => {
+  return apicall(forecastEndPiont(params));
+};
+
+export const fetchSearchEndPiont = params => {
+  return apicall(searchEndPiont(params));
+};
